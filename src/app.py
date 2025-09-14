3e2bed1ba3866e3774f446e2e3d75b648bd0926a
shashank-248586 <shashankshekhartripathy36@gmail.com>
2025-09-14 13:14:36 +0530

    commit

--- src/app.py
+++ src/app.py
@@ -1,10 +1,18 @@
-import detection
-from detection.cameraoutput import rpicam
-from detection.hailoio import hailomodel
-#b = hailomodel()
-#b.hailoconfigure()
-a = rpicam()
-a.setup_camera()
-#print(a.capture_frames())
-for i in range(1):
-    print(a.capture_frames())
+# import detection
+# from detection.cameraoutput import rpicam
+# from detection.hailoio import hailomodel
+# from queue import Queue
+# A = Queue(maxsize= 10)
+# B = Queue(maxsize=3)
+
+# #b = hailomodel()
+# #b.hailoconfigure()
+# a = rpicam()
+# a.setup_camera()
+# #print(a.capture_frames())
+# for i in range(1):
+#     print(a.capture_frames())
+import postprocess.example as p
+import numpy as np
+arr = np.array([1.0, 2.0, 3.0], dtype=np.float32)
+print(p.scale_array(arr, 5.0))
